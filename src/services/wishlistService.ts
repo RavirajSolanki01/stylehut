c384723dd3fe69ca32752aca54f75477fa10de9d
Neha Makwana <neha.makwana@tatvasoft.com>
2025-05-09 13:11:44 +0530

    build issue fixed

--- src/services/wishlistService.ts
+++ src/services/wishlistService.ts
@@ -16,6 +16,6 @@ export const getWishlist = (params: { page?: number; pageSize?: number }) => {
   return api.get(GET_WISHLIST_API_ENDPOINT, { params, withAuth: true });
 };
 
-export const postWishlist = (payload: { product_id?: number, isSoftAdd: boolean }) => {
+export const postWishlist = (payload: { product_id?: number, isSoftAdd?: boolean }) => {
   return api.post(POST_WISHLIST_API_ENDPOINT, payload, { withAuth: true });
 };
