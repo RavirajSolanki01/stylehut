8bc522c4bba28a6459b4d8bde927427c75a7f291
Raj Daslaniya <rajdaslaniya1234@gmail.com>
2025-06-03 10:29:08 +0530

    show 5 skeleton in shop by category

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,31 +15,23 @@ import {
   Slide5,
 } from "../../assets";
 import { getLandingPageShopByCategory } from "../../services/landingPageService";
-import {
-  AdminSettingsCategory,
-  LandingPageShopByCategory,
-} from "../../utils/types";
+import { AdminSettingsCategory, LandingPageShopByCategory } from "../../utils/types";
 import { SkeletonShopByCategory } from "../../components/ShopByCategoryCard/SkeletonShopByCategory";
 import ShopByCategoryCard from "../../components/ShopByCategoryCard";
 
 export const HomePage: React.FC = () => {
   const [currentIndex, setCurrentIndex] = useState<number>(0);
   const images = [Slide1, Slide5, Slide2, Slide3, Slide4];
-  const [shopByCategoryData, setShopByCategoryData] = useState<
-    LandingPageShopByCategory[]
-  >([]);
-  const [adminSettingCardConfigure, setAdminSettingCardConfigure] =
-    useState<AdminSettingsCategory>({
-      fontColor: "#004300",
-      cardColor: "#d3e2fe",
-    });
+  const [shopByCategoryData, setShopByCategoryData] = useState<LandingPageShopByCategory[]>([]);
+  const [adminSettingCardConfigure, setAdminSettingCardConfigure] = useState<AdminSettingsCategory>({
+    fontColor: "#004300",
+    cardColor: "#d3e2fe",
+  });
   const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     const timer = setInterval(() => {
-      setCurrentIndex((prevIndex) =>
-        prevIndex === images.length - 1 ? 0 : prevIndex + 1
-      );
+      setCurrentIndex((prevIndex) => (prevIndex === images.length - 1 ? 0 : prevIndex + 1));
     }, 3000);
 
     return () => clearInterval(timer);
@@ -62,24 +54,12 @@ export const HomePage: React.FC = () => {
       <div className="flex justify-center max-w-[1300px] w-full mx-auto items-center overflow-auto">
         <div className="w-full px-8 responsive-home-page">
           <div className="flex flex-col gap-3 mb-5">
-            <img
-              src={FashionCarnivalImage}
-              alt="FashionCarnivalImage"
-              className="cursor-pointer fashion-carnival-image"
-            />
+            <img src={FashionCarnivalImage} alt="FashionCarnivalImage" className="cursor-pointer fashion-carnival-image" />
             <img src={PaymentMethods} alt="PaymentMethods" />
             <img src={CouponsTitle} alt="CouponsTitle" />
             <div className="flex flex-wrap gap-2 max-w-[1300px] justify-center">
-              <img
-                className="max-h-[126px] coupons"
-                src={Coupon1}
-                alt="Coupon1"
-              />
-              <img
-                className="max-h-[126px] coupons"
-                src={Coupon2}
-                alt="Coupon2"
-              />
+              <img className="max-h-[126px] coupons" src={Coupon1} alt="Coupon1" />
+              <img className="max-h-[126px] coupons" src={Coupon2} alt="Coupon2" />
             </div>
           </div>
           <div className="relative w-full h-full mx-auto overflow-hidden">
@@ -98,9 +78,7 @@ export const HomePage: React.FC = () => {
                   key={index}
                   onClick={() => setCurrentIndex(index)}
                   className={`w-full max-w-[1px] p-[3px] mt-[10px] h-full max-h-[1px] rounded-[50%] hover:border-transparent focus:outline-none focus:border-transparent ${
-                    index === currentIndex
-                      ? "bg-[#83838f] border border-[#83838f]"
-                      : "bg-[#d7d7d7] border border-[#efefef]"
+                    index === currentIndex ? "bg-[#83838f] border border-[#83838f]" : "bg-[#d7d7d7] border border-[#efefef]"
                   }`}
                 />
               ))}
@@ -113,11 +91,7 @@ export const HomePage: React.FC = () => {
 
                   <div className="w-full flex flex-wrap justify-center gap-10 my-6 ">
                     {shopByCategoryData.map((item: any, index: number) => (
-                      <ShopByCategoryCard
-                        key={`${item.id}-${index}`}
-                        product={item}
-                        adminSettingCardConfigure={adminSettingCardConfigure}
-                      />
+                      <ShopByCategoryCard key={`${item.id}-${index}`} product={item} adminSettingCardConfigure={adminSettingCardConfigure} />
                     ))}
                   </div>
                 </>
@@ -126,18 +100,14 @@ export const HomePage: React.FC = () => {
                 <>
                   <img src={ShopByCategory} alt="SkeletonProduct" />
                   <div className="w-full flex flex-wrap justify-center gap-10 my-6 ">
-                    {Array.from({ length: 4 }).map((_, index) => (
+                    {Array.from({ length: 5 }).map((_, index) => (
                       <SkeletonShopByCategory key={index} />
                     ))}
                   </div>
                 </>
               )}
 
-              <img
-                src={KnockoutOffers}
-                alt="KnockoutOffers"
-                className="cursor-pointer"
-              />
+              <img src={KnockoutOffers} alt="KnockoutOffers" className="cursor-pointer" />
             </div>
           </div>
         </div>
