3ef61de282a6bf1fe33ba0b75d8f0a59a1c6454d
Raj Daslaniya <rajdaslaniya1234@gmail.com>
2025-06-13 12:36:54 +0530

    fix ui issue

--- src/components/ProductDetails/SizeChart/index.tsx
+++ src/components/ProductDetails/SizeChart/index.tsx
@@ -8,11 +8,7 @@ import Favorite from "@mui/icons-material/Favorite";
 import { ProductStockItem } from "../../../utils/types";
 import { ArrowRightAltSharp } from "@mui/icons-material";
 import { useNavigate } from "react-router-dom";
-import {
-  BottomWearSizeChart,
-  FootWearSizeChart,
-  TopWearSizeChart,
-} from "../../../assets";
+import { BottomWearSizeChart, FootWearSizeChart, TopWearSizeChart } from "../../../assets";
 interface ISizeChartProps {
   images: string[];
   productName: string;
@@ -61,25 +57,19 @@ const SizeChart: React.FC<ISizeChartProps> = ({
     label: item.size_data?.size,
     available: item.quantity > 0,
     measurements:
-      item.size_data?.size_chart_data?.map(
-        (chart: { size_field_name: any; size_field_value: string }) => ({
-          name: chart.size_field_name,
-          minValue: parseFloat(chart.size_field_value),
-        })
-      ) || [],
+      item.size_data?.size_chart_data?.map((chart: { size_field_name: any; size_field_value: string }) => ({
+        name: chart.size_field_name,
+        minValue: parseFloat(chart.size_field_value),
+      })) || [],
   }));
 
   const measurements =
-    sizesData[0]?.size_data?.size_chart_data?.map(
-      (m: { size_field_name: any }) => ({
-        name: m.size_field_name,
-        unit: !sizesData[0]?.size_data?.is_cm ? "Inches" : "Cm",
-      })
-    ) || [];
+    sizesData[0]?.size_data?.size_chart_data?.map((m: { size_field_name: any }) => ({
+      name: m.size_field_name,
+      unit: !sizesData[0]?.size_data?.is_cm ? "Inches" : "Cm",
+    })) || [];
 
-  const [measurementUnit, setMeasurementUnit] = useState<string>(
-    measurements[0]?.unit ?? "Inches"
-  );
+  const [measurementUnit, setMeasurementUnit] = useState<string>(measurements[0]?.unit ?? "Inches");
 
   const inchesToCentimeters = (unit: string, amount: number) => {
     return unit === "Inches" ? amount.toFixed(1) : (amount * 2.54).toFixed(1);
@@ -89,9 +79,7 @@ const SizeChart: React.FC<ISizeChartProps> = ({
     setSelectedSize(sizeId === selectedSize ? 0 : sizeId);
   };
 
-  const discountedPrice = Math.round(
-    Number(price.toString().replace(/[^\d]/g, "")) * (1 - discount / 100)
-  );
+  const discountedPrice = Math.round(Number(price.toString().replace(/[^\d]/g, "")) * (1 - discount / 100));
 
   const renderImage = () => {
     switch (subCategory.name) {
@@ -115,23 +103,13 @@ const SizeChart: React.FC<ISizeChartProps> = ({
         </div>
 
         <div className="flex items-start space-x-8 px-4 mt-2 mb-8">
-          <img
-            src={images?.[0] || ""}
-            alt="Product"
-            className="w-40 h-50 object-cover"
-          />
+          <img src={images?.[0] || ""} alt="Product" className="w-40 h-50 object-cover" />
           <div>
             <h2 className="text-xl font-semibold">{brandName}</h2>
             <p className="text-lg text-[#282c3f] mt-1 mb-1">{productName}</p>
             <p className="mt-1 text-base font-semibold text-[#282c3f]">
-              ₹{discountedPrice}{" "}
-              <span className="line-through text-[#94969f] opacity-80 text-base mx-1">
-                ₹{price}
-              </span>
-              <span className="text-[#ff905a] font-semibold text-base">
-                {" "}
-                ({discount}% OFF)
-              </span>
+              ₹{discountedPrice} <span className="line-through text-[#94969f] opacity-80 text-base mx-1">₹{price}</span>
+              <span className="text-[#ff905a] font-semibold text-base"> ({discount}% OFF)</span>
             </p>
           </div>
         </div>
@@ -143,9 +121,7 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                 key={tab.id}
                 onClick={() => setActiveTab(tab.id)}
                 className={`flex-1 text-center py-3 h-fit cursor-pointer text-lg ${
-                  activeTab === tab.id
-                    ? "text-[#3880FF] border-b-2 border-[#3880FF] font-semibold"
-                    : "text-black"
+                  activeTab === tab.id ? "text-[#3880FF] border-b-2 border-[#3880FF] font-semibold" : "text-black"
                 }`}
               >
                 {tab.label}
@@ -158,21 +134,13 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                 <div className="border border-[#eaeaec] rounded-full p-[2px] flex items-center">
                   <button
                     onClick={() => setMeasurementUnit("Inches")}
-                    className={`cursor-pointer px-2 py-1 ${
-                      measurementUnit == "Inches"
-                        ? "text-white bg-black  rounded-full"
-                        : ""
-                    }`}
+                    className={`cursor-pointer px-2 py-1 ${measurementUnit == "Inches" ? "text-white bg-black  rounded-full" : ""}`}
                   >
                     in
                   </button>
                   <button
                     onClick={() => setMeasurementUnit("Cm")}
-                    className={`cursor-pointer px-2 py-1 ${
-                      measurementUnit == "Cm"
-                        ? "text-white bg-black rounded-full"
-                        : ""
-                    }`}
+                    className={`cursor-pointer px-2 py-1 ${measurementUnit == "Cm" ? "text-white bg-black rounded-full" : ""}`}
                   >
                     cm
                   </button>
@@ -185,14 +153,9 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                       <thead>
                         <tr className="border border-[#eaeaec]">
                           <th className="sizeChartWeb-newCell sticky left-0 bg-white z-20"></th>
-                          <th className="sizeChartWeb-newCell text-base font-[400] sticky left-[40px] bg-white z-20">
-                            Size
-                          </th>
+                          <th className="sizeChartWeb-newCell text-base font-[400] sticky left-[40px] bg-white z-20">Size</th>
                           {measurements.map((measurement: { name: any }) => (
-                            <th
-                              className="sizeChartWeb-newCell text-base font-[400]"
-                              key={String(measurement.name)}
-                            >
+                            <th className="sizeChartWeb-newCell text-base font-[400]" key={String(measurement.name)}>
                               {measurement.name} ({measurementUnit.slice(0, 2)})
                             </th>
                           ))}
@@ -200,14 +163,7 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                       </thead>
                       <tbody>
                         {sizes.map((size: any) => (
-                          <tr
-                            className={`border border-[#eaeaec] ${
-                              size.available
-                                ? ""
-                                : "opacity-40 pointer-events-none"
-                            }`}
-                            key={size.skuId}
-                          >
+                          <tr className={`border border-[#eaeaec] ${size.available ? "" : "opacity-40 pointer-events-none"}`} key={size.skuId}>
                             <td className="sizeChartWeb-newCell text-base sticky left-0 bg-white z-10">
                               <Radio
                                 checked={selectedSize === size.label}
@@ -226,31 +182,14 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                               />
                             </td>
 
-                            <td
-                              className={`sizeChartWeb-newCell text-base sticky left-[40px] bg-white z-10 ${
-                                size.available ? "" : "line-through"
-                              }`}
-                            >
+                            <td className={`sizeChartWeb-newCell text-base sticky left-[40px] bg-white z-10 ${size.available ? "" : "line-through"}`}>
                               {size.label}
                             </td>
-                            {size.measurements.map(
-                              (
-                                measurement: { minValue: any },
-                                index: React.Key
-                              ) => (
-                                <td
-                                  className={`sizeChartWeb-newCell text-base ${
-                                    size.available ? "" : "line-through"
-                                  }`}
-                                  key={index}
-                                >
-                                  {inchesToCentimeters(
-                                    measurementUnit,
-                                    Number(measurement.minValue)
-                                  )}
-                                </td>
-                              )
-                            )}
+                            {size.measurements.map((measurement: { minValue: any }, index: React.Key) => (
+                              <td className={`sizeChartWeb-newCell text-base ${size.available ? "" : "line-through"}`} key={index}>
+                                {inchesToCentimeters(measurementUnit, Number(measurement.minValue))}
+                              </td>
+                            ))}
                           </tr>
                         ))}
                       </tbody>
@@ -258,7 +197,7 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                   </div>
                 </div>
               </div>
-              <div className="p-4 flex items-center gap-3 sticky bottom-0 bg-white  shadow-[0_0px_24px_[#eaeaec]] z-[1]">
+              <div className="p-4 flex items-center gap-3 sticky bottom-0 bg-white  shadow-[0_0px_24px_[#eaeaec]] z-[10]">
                 {isAddedToCart ? (
                   <button
                     onClick={() => navigate("/cart")}
@@ -272,9 +211,7 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                     onClick={() => addToCart?.(selectedSize)}
                     disabled={!selectedSize}
                     className={`cursor-pointer ${
-                      selectedSize
-                        ? "bg-[#3880FF] hover:bg-[#3880FF]"
-                        : "bg-gray-300 cursor-not-allowed"
+                      selectedSize ? "bg-[#3880FF] hover:bg-[#3880FF]" : "bg-gray-300 cursor-not-allowed"
                     } w-full text-[#fff] font-bold text-[14px] rounded-[4px] flex items-center justify-center gap-[6px] hover:border-transparent h-10`}
                   >
                     <ShoppingBagOutlined className="!w-[20px] !h-[20px]" />
@@ -289,16 +226,9 @@ const SizeChart: React.FC<ISizeChartProps> = ({
                   } cursor-pointer border border-[#d4d5d9] text-[#282c3f] bg-[#fff] font-bold text-[14px]  rounded-[4px] flex items-center justify-center gap-[6px] hover:border-[#535766] w-full`}
                 >
                   {isWishlisted ? (
-                    <Favorite
-                      fill="bg-[#3880FF]"
-                      className="text-[#3880FF] "
-                      style={{ fontSize: "1rem" }}
-                    />
+                    <Favorite fill="bg-[#3880FF]" className="text-[#3880FF] " style={{ fontSize: "1rem" }} />
                   ) : (
-                    <FavoriteBorderIcon
-                      className="!w-[20px] !h-[20px]"
-                      style={{ fontSize: "1rem" }}
-                    />
+                    <FavoriteBorderIcon className="!w-[20px] !h-[20px]" style={{ fontSize: "1rem" }} />
                   )}
                   <span>WISHLIST</span>
                 </button>
@@ -306,18 +236,10 @@ const SizeChart: React.FC<ISizeChartProps> = ({
             </div>
           ) : (
             <div className="transition-transform duration-300 ease-out pt-4 translate-y-0">
-              <div className="text-center text-sm mt-2">
-                * Garment Measurements in {measurementUnit}
-              </div>
+              <div className="text-center text-sm mt-2">* Garment Measurements in {measurementUnit}</div>
               <div className="text-center mt-4">
-                <div className="px-4 text-[#282c3f] text-left font-semibold">
-                  How to measure yourself
-                </div>
-                <img
-                  src={renderImage()}
-                  alt="Size Representation"
-                  className="max-w-full w-auto h-auto mt-4 mx-auto mb-3  rounded-[4px]"
-                />
+                <div className="px-4 text-[#282c3f] text-left font-semibold">How to measure yourself</div>
+                <img src={renderImage()} alt="Size Representation" className="max-w-full w-auto h-auto mt-4 mx-auto mb-3  rounded-[4px]" />
               </div>
             </div>
           )}
